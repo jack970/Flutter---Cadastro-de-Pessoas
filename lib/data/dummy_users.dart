0e86669806311e937932954dd013832a75258158
Matusalem Garcia <matusalem@workinformatica.com.br>
2020-11-11 18:04:51 -0300

    First commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: "Maria",
+    email: "maria@alunos.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png"
+  ),
+  "2": const User(
+    id: "2",
+    name: "Rafael", 
+    email: "rafael@alunos.com.br", 
+    avatarUrl: "https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png"
+  ),
+  "3": const User(
+    id:"3",
+    name: "Fernanda", 
+    email: "fernanda@zymail.com", 
+    avatarUrl: "https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png"),
+  "4": const User(
+    id:"4",
+    name: "Guilherme", 
+    email: "guigui@abcd.com", 
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png")
+};
