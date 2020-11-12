ad0df909a12cb7a22e2696c2a0c6deecbf4f8fa0
Matusalem Garcia <matusalem@workinformatica.com.br>
2020-11-12 16:27:16 -0300

    fix problems

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/provider/users.dart';
 import 'package:provider/provider.dart ';
-import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/routes/app_routes.dart';
 
